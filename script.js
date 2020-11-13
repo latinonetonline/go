2e90086a0c9dd7a0916e9c0e98702232c0b9edd4
Latino .NET Online <62702977+latinonetonline@users.noreply.github.com>
2020-11-13 14:48:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var hash = location.pathname.split('/')[1]
-fetch(`https://raw.githubusercontent.com/latinonetonline/linksdb/master/${hash}`)
+var linkname = location.pathname.split('/')[1]
+fetch(`https://raw.githubusercontent.com/latinonetonline/linksdb/master/links/${linkname}`)
 .then(data => data.json())
     .then(data => window.location.href = data.url)
 
